8fb9ab724a55d63b50bf9869c71ddb44f3763cb1
Jakub Chadim <ja@jakubchadim.cz>
2020-08-10 13:56:20 +0200

    Box component

---
+++ web/src/components/ui/box/box.stories.tsx
@@ -0,0 +1,31 @@
+import { select } from '@storybook/addon-knobs'
+import React from 'react'
+import styled from 'styled-components'
+import { generateParagraph } from '../../../utils/storiesUtils'
+import { BlockColor } from '../../block/color/color'
+import UiBox from './box'
+
+const Offset = styled.div`
+  padding: 10rem;
+`
+
+export default {
+  title: 'Ui'
+}
+
+export const Box = () => (
+  <Offset>
+    <UiBox
+      backgroundColor={select('Background color', BlockColor, BlockColor.WHITE)}
+      textColor={select('Text color', BlockColor, BlockColor.BLACK)}
+    >
+      <UiBox.Header>
+        <h2>Title</h2>
+        <h4>subtitle</h4>
+      </UiBox.Header>
+      <UiBox.Content>
+        <p>{generateParagraph()}</p>
+      </UiBox.Content>
+    </UiBox>
+  </Offset>
+)
---
+++ web/src/components/ui/box/box.tsx
@@ -0,0 +1,40 @@
+import styled, { css } from 'styled-components'
+import { BlockColorPalette } from '../../block/color/color'
+import { getColorFromPalette } from '../../block/color/utils'
+import { createUiComponent } from '../utils'
+
+const UiBox = styled.div<BlockColorPalette>`
+  background: ${(p) =>
+    getColorFromPalette(
+      p.backgroundColor,
+      p.theme,
+      false,
+      p.theme.color.white1
+    )};
+  color: ${(p) =>
+    getColorFromPalette(p.textColor, p.theme, false, p.theme.color.black1)};
+  box-shadow: ${(p) => p.theme.shadow.lift};
+  border-radius: ${(p) => p.theme.radius.medium};
+`
+
+const Header = styled.div`
+  padding: ${(p) => p.theme.spacing(5, 6, 0)};
+  margin-bottom: ${(p) => p.theme.spacing(4)};
+`
+
+const Content = styled.div`
+  padding: ${(p) => p.theme.spacing(5, 6)};
+
+  ${Header} + & {
+    padding-top: ${(p) => p.theme.spacing(0)};
+  }
+
+  p:last-child {
+    margin-bottom: 0;
+  }
+`
+
+export default createUiComponent(UiBox, {
+  Header,
+  Content
+})
