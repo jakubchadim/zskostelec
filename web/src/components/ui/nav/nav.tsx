ab60599e1f9675f1aed7172418c5ef45f29f2626
Jakub Chadim <ja@jakubchadim.cz>
2020-11-10 22:36:51 +0100

    Release v0.0.7

--- web/src/components/ui/nav/nav.tsx
+++ web/src/components/ui/nav/nav.tsx
@@ -21,7 +21,7 @@ const Submenu = styled.div<{ last?: boolean }>`
   position: absolute;
   border-radius: ${(p) => p.theme.radius.medium};
   box-shadow: ${(p) => p.theme.shadow.large};
-  opacity: .5;
+  opacity: 0.5;
   transition: opacity 0.3s, margin 0.2s ease-in-out;
   user-select: none;
   visibility: hidden;
@@ -41,7 +41,7 @@ const menuItemCss = css<MenuItemProps>`
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
-    right: ${p => p.theme.spacing(1)};
+    right: ${(p) => p.theme.spacing(1)};
     opacity: 0.7;
   }
 `
@@ -65,36 +65,38 @@ type UiNavProps = {
 const UiNav = styled.ul<UiNavProps>`
   list-style: none;
   padding: 0;
-  display: ${p => p.fill ? 'block' : 'inline-block'};
-  margin: ${p => p.inline ? p.theme.spacing(0, -2) : p.theme.spacing(p.simple ? 0 : -1, 0)};
-  display: ${p => p.inline ? 'flex' : undefined};
-  
+  display: ${(p) => (p.fill ? 'block' : 'inline-block')};
+  margin: ${(p) =>
+    p.inline ? p.theme.spacing(0, -2) : p.theme.spacing(p.simple ? 0 : -1, 0)};
+  display: ${(p) => (p.inline ? 'flex' : undefined)};
+
   & > ${Item} {
-    margin: ${p => p.inline ? p.theme.spacing(0, 2) : p.theme.spacing(p.simple ? 0 : 1, 0)};
+    margin: ${(p) =>
+      p.inline ? p.theme.spacing(0, 2) : p.theme.spacing(p.simple ? 0 : 1, 0)};
 
     & > ${Link} {
-      text-decoration: ${p => p.simple ? 'none' : undefined};
-      
+      text-decoration: ${(p) => (p.simple ? 'none' : undefined)};
+
       &:hover {
-        color: ${p => p.transparent ? 'inherit' : undefined};
-        text-decoration: ${p => p.transparent ? 'underline' : undefined};
+        color: ${(p) => (p.transparent ? 'inherit' : undefined)};
+        text-decoration: ${(p) => (p.transparent ? 'underline' : undefined)};
       }
     }
-    
+
     & > ${Submenu} {
-      top: ${p => p.inline ? '100%' : 0};
+      top: ${(p) => (p.inline ? '100%' : 0)};
       left: ${(p) => (p.inline ? '50%' : '100%')};
       transform: translateX(${(p) => (p.inline ? '-50%' : '0')});
     }
-    
+
     &:last-child {
       & > ${Submenu} {
-        left: ${p => p.inline ? 'inherit' : undefined};
-        right: ${p => p.inline ? 0 : undefined};;
-        transform: ${p => p.inline ? 'translateX(0)' : undefined};
+        left: ${(p) => (p.inline ? 'inherit' : undefined)};
+        right: ${(p) => (p.inline ? 0 : undefined)};
+        transform: ${(p) => (p.inline ? 'translateX(0)' : undefined)};
       }
     }
-    
+
     &:hover {
       & > ${Submenu} {
         opacity: 1;
