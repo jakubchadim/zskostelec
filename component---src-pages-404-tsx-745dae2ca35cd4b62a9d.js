185b703e0a9b5fcf6a2e6a4096f42ebda5a345f2
Jakub Chadim <ja@jakubchadim.cz>
2020-11-21 18:47:14 +0100

    Deploy v0.0.8

---
+++ component---src-pages-404-tsx-745dae2ca35cd4b62a9d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),s=n("B7F5");t.default=function(){return u.a.createElement(s.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-745dae2ca35cd4b62a9d.js.map
